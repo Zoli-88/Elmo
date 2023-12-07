9149b630d197a572082b41cb0bf495146ac48495
Hegedus Zoltan <zoltan.hegedus88@gmail.com>
2023-12-07 23:46:43 +0200

    set eyes to look straight if mouse leaves window

--- main.js
+++ main.js
@@ -1,24 +1,33 @@
-document.addEventListener("mousemove", moveEyes);
+
 const pupils = document.querySelectorAll(".pupils");
 const eyeContainer = document.querySelector(".eyes");
 const eyeWidth = eyeContainer.offsetWidth;
 const eyeHeight = eyeContainer.offsetHeight;
 const eyeCenterX = eyeContainer.offsetLeft + eyeWidth / 2;
 const eyeCenterY = eyeContainer.offsetTop + eyeHeight / 2;
-const offset = 100;
+const offset = 0.7;
 
 function moveEyes(e) {
     const mouseX = e.clientX;
     const mouseY = e.clientY;
     const xDistance = mouseX - eyeCenterX;
-    const xMaxDistance = eyeWidth / 2 + offset;
+    const xMaxDistance = eyeWidth / 2;
     const yDistance = mouseY - eyeCenterY;
-    const yMaxDistance = eyeHeight / 2 + offset;
+    const yMaxDistance = eyeHeight / 2;
 
     const xPercentage = Math.min(Math.max(xDistance / xMaxDistance, -1), 1);
     const yPercentage = Math.min(Math.max(yDistance / yMaxDistance, -1), 1);
 
     pupils.forEach((pupil) => {
-        pupil.style.transform = `translate(${xPercentage * 10}px, ${yPercentage * 10}px)`;
+        pupil.style.transform = `translate(${xPercentage * 10 * offset}px, ${yPercentage * 10 * offset}px)`;
     });
 }
+
+function lookStraight() {
+    pupils.forEach((pupil) => {
+        pupil.style.transform = `translate(0, 0)`;
+    });
+}
+
+document.addEventListener("mousemove", moveEyes);
+document.addEventListener("mouseleave", lookStraight);
